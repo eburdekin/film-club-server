c7e22751461b03296566cfe7b0401eb995793d6a
Eileen <eileenburdekin@proton.me>
2024-02-13 12:58:54 -0800

    change filter for similarbymovieid route

--- server/app.py
+++ server/app.py
@@ -4,7 +4,7 @@ from flask import request, session, make_response, jsonify, render_template
 from flask_restful import Resource
 from functools import wraps
 from marshmallow import Schema, fields, validate, ValidationError
-from sqlalchemy import and_
+from sqlalchemy import and_, not_
 
 # Local imports
 from models import db, Movie, Role, User, Club, ScreeningRoom, Post, Rating
@@ -217,7 +217,11 @@ class SimilarMoviesByGenre(Resource):
 
         # Get other movies with the same genre(s)
         filters = [Movie.genres.any(id=genre.id) for genre in movie_genres]
-        similar_movies = Movie.query.filter(and_(*filters)).limit(5).all()
+        similar_movies = (
+            Movie.query.filter(and_(*filters), not_(Movie.id == movie_id))
+            .limit(6)
+            .all()
+        )
 
         # Serialize the data
         movie_schema = MovieSchema(many=True)
